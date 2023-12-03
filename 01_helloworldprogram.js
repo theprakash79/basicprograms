7206a1ef627d28e43c22c1bf6590fbc3507c1793
theprakash <theprakash79@gmail.com>
2023-12-03 08:14:25 +0530

    commit message

---
+++ 01_helloworldprogram.js
@@ -0,0 +1,14 @@
+
+function main(){
+    // write Hello world programs
+    console.log("Hello world!");
+}
+main();
+/*
+    output
+    --------
+    Hello world!
+*/
+
+// $ git config --global user.name "John Doe"
+// $ git config --global user.email johndoe@example.com
